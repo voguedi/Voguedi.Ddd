591f2a1913e87fe9d7518ebf51172de75c1de5b2
张俊亚 <voguedi@gmail.com>
2018-11-19 14:23:09 +0800

    修改：更新 Utils 包，实体、聚合根移除构造方法

--- src/Voguedi.Ddd/Voguedi/Domain/AggregateRoots/AggregateRoot.cs
+++ src/Voguedi.Ddd/Voguedi/Domain/AggregateRoots/AggregateRoot.cs
@@ -2,14 +2,5 @@
 
 namespace Voguedi.Domain.AggregateRoots
 {
-    public abstract class AggregateRoot<TIdentity> : Entity<TIdentity>, IAggregateRoot<TIdentity>
-    {
-        #region Ctors
-
-        protected AggregateRoot() { }
-
-        protected AggregateRoot(TIdentity id) : base(id) { }
-
-        #endregion
-    }
+    public abstract class AggregateRoot<TIdentity> : Entity<TIdentity>, IAggregateRoot<TIdentity> { }
 }
--- src/Voguedi.Ddd/Voguedi/Domain/Entities/Entity.cs
+++ src/Voguedi.Ddd/Voguedi/Domain/Entities/Entity.cs
@@ -5,14 +5,6 @@ namespace Voguedi.Domain.Entities
 {
     public abstract class Entity<TIdentity> : ValueObject, IEntity<TIdentity>
     {
-        #region Ctors
-
-        protected Entity() { }
-
-        protected Entity(TIdentity id) => Id = id;
-
-        #endregion
-
         #region ValueObject
 
         protected override IEnumerable<object> GetEqualityPropertryValues()
